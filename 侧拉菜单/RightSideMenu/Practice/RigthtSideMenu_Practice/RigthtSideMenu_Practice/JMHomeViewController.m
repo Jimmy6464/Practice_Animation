600405f81b6b5d7410754c909748ed2547212c05
Jimmy <jimmy@DevMacBookPro.local>
2016-04-25 11:03:50 +0800

    updated 11:04

--- 侧拉菜单/RightSideMenu/Practice/RigthtSideMenu_Practice/RigthtSideMenu_Practice/JMHomeViewController.m
+++ 侧拉菜单/RightSideMenu/Practice/RigthtSideMenu_Practice/RigthtSideMenu_Practice/JMHomeViewController.m
@@ -8,18 +8,27 @@
 
 #import "JMHomeViewController.h"
 #import "JMNavigationController.h"
+#import "UIViewController+JMSlideMainViewController.h"
+#import "JMSecondViewController.h"
 @implementation JMHomeViewController
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.title = @"Home";
     self.view.backgroundColor = [ UIColor whiteColor];
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"menu"] style:UIBarButtonItemStylePlain target:(JMNavigationController *)self.navigationController action:@selector(showMenu)];
+    UIImage *image = [UIImage imageNamed:@"menu"];
+    UIButton *btn  = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setImage:image forState:UIControlStateNormal];
+    btn.bounds = CGRectMake(0, 0, 30, 25);
+    [btn addTarget:(JMNavigationController *)self.navigationController action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:btn];
     
     UIImageView *imageView = [[UIImageView alloc]initWithFrame:self.view.bounds];
     imageView.image = [ UIImage imageNamed:@"background"];
     imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     [self.view addSubview:imageView];
+    
+    [self re_displayController:[JMSecondViewController new] frame:CGRectMake(100, 100, 200, 400)];
 }
 @end
--- 侧拉菜单/RightSideMenu/Practice/RigthtSideMenu_Practice/RigthtSideMenu_Practice/JMSecondViewController.m
+++ 侧拉菜单/RightSideMenu/Practice/RigthtSideMenu_Practice/RigthtSideMenu_Practice/JMSecondViewController.m
@@ -8,14 +8,54 @@
 
 #import "JMSecondViewController.h"
 #import "JMNavigationController.h"
+#import "UIViewController+JMSlideMainViewController.h"
+@interface JMSecondViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic, weak) UITableView * tableView;
+@property (nonatomic, strong) NSArray *dataSources;
+@end
 @implementation JMSecondViewController
+- (NSArray *)dataSources
+{
+    if (_dataSources == nil) {
+        _dataSources = @[@"row1",@"row1",@"row1",@"row1",@"row1",@"row1"];
+    }
+    return _dataSources;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.title = @"Second";
     self.view.backgroundColor = [UIColor cyanColor];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"menu"] style:UIBarButtonItemStylePlain target:(JMNavigationController *)self.navigationController action:@selector(showMenu)];
+    [self initializedSubviews];
+}
+#pragma  mark - initialized subviews
+- (void)initializedSubviews
+{
+    UITableView *tabelView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    tabelView.delegate = self;
+    tabelView.dataSource = self;
+    [self.view addSubview:tabelView];
+    _tableView = tabelView;
 }
 
-
+#pragma  mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSources ? self.dataSources.count:0;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text = self.dataSources[indexPath.row];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self re_hideController:self];
+//    [self.navigationController  pushViewController:[JMSecondViewController new] animated:YES];
+}
 @end
